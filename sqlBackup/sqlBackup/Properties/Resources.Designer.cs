d0156fd630339feb9d9587d5673015f5955760fa
sotiris-rafail <smoschop@gmail.com>
2015-11-08 17:07:05 +0200

    Neo project
    to neo project pernw back edw gia na exw an pextei kati

--- sqlBackup/sqlBackup/Properties/Resources.Designer.cs
+++ sqlBackup/sqlBackup/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BackUpDb.Properties
+namespace sqlBackup.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace BackUpDb.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BackUpDb.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("sqlBackup.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
